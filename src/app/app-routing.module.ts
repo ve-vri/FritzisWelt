75a3d7b414935b585d55ad709896b065bdb3061b
Verena Ade <verena-ade@gmx.de>
2021-08-14 11:29:34 +0200

    added bootstrap and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './_components/characters/characters.component';
+import { HomeComponent } from './_components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component:HomeComponent},
+  {path: "characters", component:CharactersComponent},
+  {path: "**", redirectTo: "/home", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
