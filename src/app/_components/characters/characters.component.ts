2f183c4c28eb343197aeb2345174ab2da960707f
Verena Ade <verena-ade@gmx.de>
2021-08-14 12:33:27 +0200

    images

--- src/app/_components/characters/characters.component.ts
+++ src/app/_components/characters/characters.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.scss']
 })
-export class CharactersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CharactersComponent  {
+  public characters = [
+    {name: "Fritzi der Frosch",description: "xxx", image: "fritzi"},
+    {name: "Schnecke Schnickelschnak",description: "xxx", image: "schnecke"},
+    {name: "Lilly die Libelle",description: "xxx", image: "lilly"},
+    {name: "Igor der Igel",description: "xxx", image: "igor"},
+    {name: "Gisela das Glühwürmchen",description: "xxx", image: "gluehwuermchen"},
+    {name: "Madame Scharmilla",description: "xxx", image: "schmetterling"},
+    {name: "Schorsch und Schantal",description: "xxx", image: "storch"},
+    {name: "Amadeus",description: "xxx", image: "amadeus"},
+  ]
 }
