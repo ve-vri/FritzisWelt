9d1bd0bf9a2f0f0b0eaea7749ea34e5e9a1e6518
Verena Ade <verena-ade@gmx.de>
2021-08-14 10:49:41 +0200

    added two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './_components/home/home.component';
+import { CharactersComponent } from './_components/characters/characters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
